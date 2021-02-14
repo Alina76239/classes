5792c49afff502945f28667fb41bccbd588f73d1
Alina62630 <kondrasheva730@mail.ru>
2021-02-14 22:48:22 +0400

    Д/З номер 3

---
+++ 10_lab/Source.cpp
@@ -0,0 +1,21 @@
+// Допишите программу, которая печатает абсолютное значение числа, введённого 
+// с клавиатуры пользователем.
+
+#include <iostream>
+
+int main() {
+    int absolute_value;
+    std::cout << "x=";
+    std::cin >> absolute_value;
+
+    if (absolute_value >= 0)
+    {
+        std::cout << absolute_value << std::endl;
+    }
+    if (absolute_value < 0)
+    {
+        absolute_value = -absolute_value;
+        std::cout << absolute_value << std::endl;
+    }
+    //std::cout << absolute_value << std::endl;
+}
